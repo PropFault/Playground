23f9defe85d0ed371fb943e85cf012413e36b38e
Tomic_Toaster_Win <tomic@tomic.cc>
2023-08-27 14:23:57 +0200

    Fixed walking direction

--- WalkingState.gd
+++ WalkingState.gd
@@ -10,4 +10,4 @@ func _state_process(delta):
 	movement.x = top_walking_speed.y * min(input.movement.x, 0.0) + top_walking_speed.z * max(input.movement.x, 0.0)
 	#F/B
 	movement.y = top_walking_speed.x * min(input.movement.y, 0.0) + top_walking_speed.w * max(input.movement.y, 0.0)
-	self.body.velocity = self.body.velocity + self.body.to_global(Vector3(movement.x, 0, movement.y)) * delta
+	self.body.velocity = self.body.velocity + Vector3(movement.x, 0, movement.y) * body.transform.basis * delta
