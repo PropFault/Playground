30fb67e405d075104d39ca8ce5a4e5b1983129d1
Tomic_Toaster_Win <tomic@tomic.cc>
2023-08-28 17:42:10 +0200

    Fixed top speed exceeding set when walking diagonally

--- Player/States/WalkingState.gd
+++ Player/States/WalkingState.gd
@@ -6,8 +6,16 @@ class_name WalkingState
 
 func _state_process(delta):
 	var movement = Vector2.ZERO
-	#L/R
-	movement.x = top_walking_speed.y * min(input.movement.x, 0.0) + top_walking_speed.z * max(input.movement.x, 0.0)
-	#F/B
-	movement.y = top_walking_speed.x * min(input.movement.y, 0.0) + top_walking_speed.w * max(input.movement.y, 0.0)
-	self.body.velocity = self.body.velocity + Vector3(movement.x, 0, movement.y) * body.transform.basis * delta
+	var f = -min(input.movement.y,0.0)
+	var b = max(input.movement.y, 0.0)
+	var l = -min(input.movement.x, 0.0)
+	var r = max(input.movement.x, 0.0)
+	
+	var top_speed = top_walking_speed.x * f + top_walking_speed.w * b + top_walking_speed.y * l + top_walking_speed.z * r
+	var divident = abs(sign(f)) + abs(sign(b)) + abs(sign(l)) + abs(sign(r))
+	if divident > 0.0:
+		top_speed = top_speed / divident
+	print("top", top_speed)
+	movement = input.movement * top_speed 
+	
+	self.body.velocity = self.body.velocity + (movement.x * body.transform.basis.x + movement.y  * body.transform.basis.z) * delta
